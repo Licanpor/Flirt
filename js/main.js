defced37863fc59cbf579cc867e16a62abe12221
LilianaCanPor <liliana.canpor@gmail.com>
2017-06-19 16:52:04 -0500

    quedó el active bouncing y el scroll al window.height

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 // console.log('\'Allo \'Allo!');
 
 $(document).ready(function(){
-	var colHeight = $('#features__menu__container').height();
-	console.log(colHeight);
-	$('.features__graphicCol').height(colHeight);
+	$('.features__graphicCol').height($(window).height());
 	console.log($('.features__graphicCol').height());
 	$('.onoffswitch-label').click(function(){
 		$('#prod__packages').slideToggle(800);
@@ -21,52 +19,35 @@ $(document).ready(function(){
 	var features = $('#features');
 	var features__mainRow = $('#features__mainRow');
 	var tl = new TimelineMax({});
-	// var tl_featuresMenu = new TimelineMAx();
 
-	// TweenMax.set('#features__mainRow', {	
-	// 	opacity: 1
-	// });
+	var allArrws = [$('#features__active1'), $('#features__active2'), $('#features__active3')];
+	var tween = TweenMax.fromTo(allArrws, 0.9, {x: -8},	{x: 8, repeat: -1, yoyo:true, ease: Power1.easeIn});
 
 	tl
-		.to('#benefits__text', 0.2, {
-			y:'-110%'
-		})
-		.fromTo('#benefits', 0.2, 
-			{css: {
-				position:'relative',
-				zIndex: 0,
-				opacity: 1
-			}}, 
-			{css: {
-				width: '100%',
-				position:'absolute',
-				zIndex: -1,
-				opacity: 0}
-		}, '-=0.15')	
-		.fromTo('#features', 0.2, 
-			{css:{
-				position: 'absolute',
-				top: 0,	
-				opacity: 0
-			}},
-			{css:{
-			position: 'relative',
-			opacity: 1,
-			overflow: 'hidden'
-		}}, '-=0.1')
-		.fromTo('#features__active1', 0.03, {css: {display:'inline-block'}}, {css: {display: 'none'}})		
-		.to('#features__graphic__1', 0.12, {y:'-110%'}, '-=0.06')
+		.play(tween)
+
+		.to('#benefits__text', 0.2, {y:'-110%'})
+		.fromTo('#benefits', 0.2,{position:'relative', zIndex: 0, opacity: 1}, { width: '100%',	position:'absolute', zIndex: -1, opacity: 0}, '-=0.15')	
+		.fromTo('#features', 0.2, {position: 'absolute', top: 0, opacity: 0}, { position: 'relative',	opacity: 1, overflow: 'hidden'}, '-=0.1')
+
+		.fromTo('#features__active1', 0.03, {css: {display:'none'}}, {css: {display:'inline-block'}}) 				
+		.from('#features__graphic__1', 0.15, {y:'110%'}, '-=0.06')
+		.to('#features__graphic__1', 0.12, {y:0}, '-=0.09')
+		.to('#features__graphic__1', 0.18, {y:'-110%'}, '+=0.06')
+		.to('#features__active1', 0.018, {display: 'none'}, '-=0.09')
 
 
 	  .fromTo('#features__active2', 0.015, {css: {display:'none'}}, {css: {display: 'inline-block'}},'-=0.045')
-	  .to('#features__graphic__2', 0.12, {y:'-100%'}, '-=0.06')
-	  .to('#features__graphic__2', 0.12, {y:'-210%'}, '+=0.1')
-	  .to('#features__active2', 0.015, {display: 'none'}, '-=0.06')
+	  .to('#features__graphic__2', 0.15, {y:'-100%'}, '-=0.09')
+	  .to('#features__graphic__2', 0.18, {y:'-210%'}, '+=0.09')
+	  .to('#features__active2', 0.018, {display: 'none'}, '-=0.09')
 
 
 	  .fromTo('#features__active3', 0.03, {css: {display:'none'}}, {css: {display: 'inline-block'}},'-=0.03')
-	  .to('#features__graphic__3', 0.12, {y:'-200%'}, '-=0.06')
+	  .to('#features__graphic__3', 0.15, {y:'-200%'}, '-=0.09')
 		
+
+
 	// ----- Init ScrollMagic
 	var controller = new ScrollMagic.Controller();
 
@@ -85,5 +66,3 @@ $(document).ready(function(){
 	})
 	.addTo(controller);
 });
-
-
