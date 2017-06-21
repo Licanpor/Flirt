7c017d16298361cfd67eb26bdd55eb5799ea201f
LilianaCanPor <liliana.canpor@gmail.com>
2017-06-21 14:11:50 -0500

    agregué ease al menu de features

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-// console.log('\'Allo \'Allo!');
-
 $(document).ready(function(){
 	$('.features__graphicCol').height($(window).height());
-	console.log($('.features__graphicCol').height());
 	$('.onoffswitch-label').click(function(){
 		$('#prod__packages').slideToggle(800);
 		$('#serv__packages').slideToggle(800);
 	});	
 
-	$(".features__container--icon a").on("click", function() {
-		$(".features__container--icon a").removeClass("features__active");
-		$(this).toggleClass("features__active");
-	});
+ //  var prev1 = $('.header__bgd').height();
+ //  console.log('altura del primer div: ' + prev1);
+ //  var prev2 = $('#performance').height();
+ //  console.log('altura del segundo div: ' + prev2);
+ //  var prevDivs = prev1 + prev2;
+ //  console.log('suman: ' + prevDivs);
+	// var sPane = prevDivs - 100;
+	// console.log(sPane);
 
 	// ----- TweenMax Animations
 	var benefits = $('#benefits');
@@ -27,26 +28,22 @@ $(document).ready(function(){
 		.play(tween)
 
 		.to('#benefits__text', 0.2, {y:'-110%'})
-		.fromTo('#benefits', 0.2,{position:'relative', zIndex: 0, opacity: 1}, { width: '100%',	position:'absolute', zIndex: -1, opacity: 0}, '-=0.15')	
-		.fromTo('#features', 0.2, {position: 'absolute', top: 0, opacity: 0}, { position: 'relative',	opacity: 1, overflow: 'hidden'}, '-=0.1')
+		.fromTo('#benefits', 0.2, {opacity: 1, zIndex: 0}, {opacity: 0}, '-=0.15')	
+		.fromTo('#features', 0.2, {position: 'absolute', top: 0, opacity: 0, zIndex: -1}, {opacity: 1, zIndex: 1, overflow: 'hidden'}, '-=0.1')
 
 		.fromTo('#features__active1', 0.03, {css: {display:'none'}}, {css: {display:'inline-block'}}) 				
-		.from('#features__graphic__1', 0.15, {y:'110%'}, '-=0.06')
+		.from('#features__graphic__1', 0.15, {ease: Circ.easeOut, y:'110%'}, '-=0.06')
 		.to('#features__graphic__1', 0.12, {y:0}, '-=0.09')
-		.to('#features__graphic__1', 0.18, {y:'-110%'}, '+=0.06')
-		.to('#features__active1', 0.018, {display: 'none'}, '-=0.09')
+		.to('#features__graphic__1', 0.21, {ease: Circ.easeIn, y:'-110%'}, '+=0.06')
+		.to('#features__active1', 0.018, {display: 'none'}, '-=0.13')
 
-
-	  .fromTo('#features__active2', 0.015, {css: {display:'none'}}, {css: {display: 'inline-block'}},'-=0.045')
-	  .to('#features__graphic__2', 0.15, {y:'-100%'}, '-=0.09')
-	  .to('#features__graphic__2', 0.18, {y:'-210%'}, '+=0.09')
-	  .to('#features__active2', 0.018, {display: 'none'}, '-=0.09')
-
+	  .fromTo('#features__active2', 0.015, {css: {display:'none'}}, {css: {display: 'inline-block'}},'-=0.07')
+	  .to('#features__graphic__2', 0.15, {ease: Circ.easeOut, y:'-100%'})//, '-=0.09'
+	  .to('#features__graphic__2', 0.21, {ease: Circ.easeIn, y:'-210%'}, '+=0.09')
+	  .to('#features__active2', 0.018, {display: 'none'}, '-=0.13')
 
 	  .fromTo('#features__active3', 0.03, {css: {display:'none'}}, {css: {display: 'inline-block'}},'-=0.03')
-	  .to('#features__graphic__3', 0.15, {y:'-200%'}, '-=0.09')
-		
-
+	  .to('#features__graphic__3', 0.18, {ease: Circ.easeOut, y:'-200%'})//, '-=0.09'
 
 	// ----- Init ScrollMagic
 	var controller = new ScrollMagic.Controller();
@@ -60,9 +57,9 @@ $(document).ready(function(){
 	.setPin('#pin__content').setTween(tl)
 	.addIndicators({
 		name: 'pin1',
-		colorTrigger: 'pink',
-		colorStart: 'aqua',
-		colorEnd: 'orange'
+		colorTrigger: 'transparent',
+		colorStart: 'transparent',
+		colorEnd: 'transparent'
 	})
 	.addTo(controller);
 });
